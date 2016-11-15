e14dd369b07b9bb004c337ef582db399e7f6696f
suem <suem@suem.suem>
2016-11-15 16:44:58 +0100

    fixed sequential pothen_fan stack implementation

--- src/pothen_fan.cpp
+++ src/pothen_fan.cpp
@@ -215,31 +215,51 @@ void pothen_fan(const Graph& g, const Vertex first_right, VertexVector& mate) {
 	const vertex_size_t  n = boost::num_vertices(g);
 	const vertex_size_t  n_right = n - first_right;
 
-	bool path_found = true;
-	bool* visited = new bool[n_right];
+	bool path_found;
+
+	vertex_size_t* visited = new vertex_size_t[n_right];
+	memset(visited, 0, sizeof(bool) * n_right); // set all visited flags to 0
+	vertex_size_t iteration = 0;
+
+    // init parent array
+    Vertex* parent = new Vertex[n_right];
+
+    // init stack
+	Vertex* stack = new Vertex[first_right];
 
     // initialize lookahead
     std::pair<AdjVertexIterator, AdjVertexIterator>* lookahead = new std::pair<AdjVertexIterator, AdjVertexIterator>[first_right];
     for (Vertex i = 0; i < first_right; i++) lookahead[i] = boost::adjacent_vertices(i, g);
 
-	while (path_found) {
+	// collect all unmatched vertices
+    std::vector<Vertex> unmatched;
+	unmatched.reserve(first_right / 2);
+	for (Vertex v = 0; v < first_right; v++) if (is_unmatched(v, g, mate)) unmatched.push_back(v);
+	size_t unmatched_size = unmatched.size();
+
+    do {
 		path_found = false; // reset path_found
 
-		memset(visited, 0, sizeof(bool) * n_right); // set all visited flags to 0
+		iteration++;
+
         // iterate over all left vertices
-		for (Vertex v = 0; v < first_right; v++) {
+
+        for (size_t i = 0; i < unmatched_size; i++) {
+			Vertex x0 = unmatched[i];
+
 			// skip if vertex is already matched
-			if (is_matched(v, g, mate)) continue;
+			if (is_matched(x0, g, mate)) continue;
 
+            bool path_found_v = find_path_la(x0, g, first_right, mate, visited, iteration, parent, lookahead, stack);
 
-			// assert: v is on the left and unmatched
-//			path_found = find_path(v, g, first_right, mate, visited) || path_found;
-//			path_found = find_path_recursive(v, g, first_right, mate, visited) || path_found;
-            path_found = find_path_la_recursive(v, g, first_right, mate, visited, lookahead) || path_found;
+            if (path_found_v && !path_found) path_found = true;
 		}
-	}
+	} while(path_found);
 
 	delete[] visited;
+	delete[] parent;
+	delete[] stack;
+    delete[] lookahead;
 }
 
 //todo find out why recursive version is faster
@@ -310,6 +330,80 @@ bool find_path(const Vertex x0, const Graph& g, Vertex first_right, VertexVector
 	}
 
 	return false;
+}
+
+bool find_path_la(
+		const Vertex x0,
+		const Graph& g, Vertex first_right,
+		VertexVector& mate,
+		vertex_size_t* visited, vertex_size_t iteration,
+		Vertex* parent,
+		std::pair<AdjVertexIterator, AdjVertexIterator>* lookahead,
+		Vertex* stack
+) {
+
+	int stack_pointer = 0;
+	stack[stack_pointer] = x0;
+
+	bool path_found = false;
+	Vertex last_y;
+
+	while(stack_pointer >= 0) {
+		Vertex x = stack[stack_pointer];
+        stack_pointer--;
+
+		// lookahead phase
+		AdjVertexIterator laStart, laEnd;
+		for (std::tie(laStart, laEnd) = lookahead[x]; laStart != laEnd; ++laStart) {
+			Vertex y = *laStart;
+			if (visited[y - first_right] != iteration && is_unmatched(y, g, mate)) {
+				visited[y - first_right] = iteration;
+				lookahead[x].first = laStart;
+                parent[y - first_right] = x;
+                path_found = true;
+				last_y = y;
+                break;
+			}
+		}
+		if (lookahead[x].first != laStart) lookahead[x].first = laStart;
+		if (path_found) break;
+
+
+		// DFS phase
+		AdjVertexIterator start, end;
+		for (std::tie(start, end) = boost::adjacent_vertices(x, g); start != end; ++start) {
+			Vertex y = *start;
+
+			if (visited[y - first_right] == iteration) continue;
+			visited[y - first_right] = iteration;
+
+			parent[y - first_right] = x;
+
+            if (is_unmatched(y,g,mate)) {
+				path_found = true;
+				last_y = y;
+				break;
+			}
+
+			stack_pointer++;
+            stack[stack_pointer] = mate[y];
+		}
+		if (path_found) break;
+
+	}
+
+	if (path_found) {
+        Vertex xp;
+		do {
+			xp = parent[last_y - first_right];
+			Vertex next_y = mate[xp];
+			mate[last_y] = xp;
+			mate[xp] = last_y;
+			last_y = next_y;
+		} while (xp != x0);
+	}
+
+	return path_found;
 }
 
 bool find_path_recursive(const Vertex x0, const Graph& g, const Vertex first_right, VertexVector& mate, bool* visited) {
